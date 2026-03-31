91c9a5d24d084a8590a865ca2383f3e5fd583259
sgonzalesgo <sgg.2020@gmail.com>
2026-03-30 22:30:05 -0400

    Updated notifications

--- routes/administration/notifications.php
+++ routes/administration/notifications.php
@@ -13,9 +13,10 @@ use App\Http\Controllers\Administration\NotificationController;
 //});
 
 Route::controller(NotificationController::class)->group(function () {
-    Route::get('/notifications', 'index');
-    Route::get('/notifications/unread-count', 'countUnread');
-    Route::patch('/notifications/chat/read', 'markChatAsRead');
-    Route::patch('/notifications/read-all', 'markAllAsRead');
-    Route::patch('/notifications/{notificationId}/read', 'markAsRead');
+    Route::get('/notifications', 'index')->middleware('permission:List notifications');
+    Route::get('/notifications/unread-count', 'countUnread')->middleware('permission:List notifications');
+
+    Route::patch('/notifications/chat/read', 'markChatAsRead')->middleware('permission:Read notifications');
+    Route::patch('/notifications/read-all', 'markAllAsRead')->middleware('permission:Read notifications');
+    Route::patch('/notifications/{notificationId}/read', 'markAsRead')->middleware('permission:Read notifications');
 });
