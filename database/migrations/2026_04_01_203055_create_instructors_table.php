7d9465a5055161b42382279333beb5006ec895a0
sgonzalesgo <sgg.2020@gmail.com>
2026-06-07 22:07:21 -0400

    Updated

--- database/migrations/2026_04_01_203055_create_instructors_table.php
+++ database/migrations/2026_04_01_203055_create_instructors_table.php
@@ -4,22 +4,63 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
         Schema::create('instructors', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->uuid('person_id');
+
+            // Relaciones
             $table->uuid('tenant_id');
+            $table->uuid('person_id');
+            $table->uuid('department_id')->nullable();
+
+            // Información institucional
             $table->string('code', 10)->unique();
-            $table->string('academic_title')->nullable(); // Dr, MSc
-            $table->string('academic_level')->nullable(); // licenciatura, maestría
-            $table->string('specialty')->nullable();      // Matemáticas, Física// Ciencias, Humanidades
+
+            // Información académica
+            $table->string('academic_title')->nullable(); // Dr., MSc., Ing.
+            $table->string('academic_level')->nullable(); // Licenciatura, Maestría, Doctorado
+            $table->string('specialty')->nullable();
+
+            // Estado
             $table->string('status')->default('active');
             $table->timestamp('status_changed_at')->nullable();
 
             $table->timestamps();
             $table->softDeletes();
+
+            /*
+             * Foreign Keys
+             */
+            $table->foreign('tenant_id')
+                ->references('id')
+                ->on('tenants')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+
+            $table->foreign('person_id')
+                ->references('id')
+                ->on('persons')
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+
+            $table->foreign('department_id')
+                ->references('id')
+                ->on('departments')
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+
+            /*
+             * Índices
+             */
+            $table->unique('person_id');
+
+            $table->index('tenant_id');
+            $table->index('department_id');
+            $table->index('status');
+            $table->index('code');
         });
     }
 
